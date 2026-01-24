8eca864735829a5db5b591e66270be477630ae92
Raushan-tryingtocode <raushanh31@gmail.com>
2026-01-24 23:47:36 +0530

    Update timer.py

--- timer.py
+++ timer.py
@@ -4,12 +4,14 @@ import time
 def timer(user_seconds):
 
     for x in range(user_seconds, -1, -1):
-        mins = x // 60
+        hours = x//3600
+        mins = (x % 3600) // 60
         secs = x % 60
-        print(f"00:{mins:02}:{secs:02}", end="\r")
+        print(f"{hours:02}:{mins:02}:{secs:02}", end="\r")
         time.sleep(1)
     print("\nTime's Up! \a")
 
 
 user_input = int(input("Set your timer (in seconds): "))
 timer(user_input)
+
